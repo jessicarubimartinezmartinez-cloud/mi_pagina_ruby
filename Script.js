97fe96286e6db8b3cfdde31ae2d273d0ec782382
jessicarubimartinezmartinez-cloud <jessicarubimartinezmartinez@gmail.com>
2026-05-12 08:45:50 -0600

    cambios
    cambios

---
+++ Script.js
@@ -0,0 +1,57 @@
+// }
+
+
+
+function mostrarMensaje() {
+    let mensaje = document.getElementById("mensaje");
+
+    mensaje.innerHTML  = "cuida y protege a los animales ";
+    mensaje.style.display = "block";
+    
+    setTimeout (() =>  {
+    mensaje.style.opacity ="1";
+    mensaje.style.transform= "translateY(0)";
+    }, 50);
+}
+function mostrarnota() {
+    let mensaje = document.getElementById("mensaje2");
+
+    mensaje.innerHTML  = "hola, mi nombre es ruby mi color favorito es el rosa  ";
+    mensaje.style.display = "block";
+    
+    setTimeout (() =>  {
+    mensaje.style.opacity ="1";
+    mensaje.style.transform= "translateY(0)";
+    }, 50);
+}
+
+
+
+
+function mostrarnota() {
+    let mensaje = document.getElementById("mensaje4");
+
+    mensaje.innerHTML  = "responde este formulario obligatoriamente ";
+    mensaje.style.display = "block";
+    
+    setTimeout (() =>  {
+    mensaje.style.opacity ="1";
+    mensaje.style.transform= "translateY(0)";
+    }, 50);
+}
+
+
+function mostrarnota() {
+    let mensaje = document.getElementById("mensaje3");
+
+    mensaje.innerHTML  = "tu mascota tiene sentimientos , cuidalo y tratalo bien ";
+    mensaje.style.display = "block";
+    
+    setTimeout (() =>  {
+    mensaje.style.opacity ="1";
+    mensaje.style.transform= "translateY(0)";
+    }, 50);
+}
+
+
+
